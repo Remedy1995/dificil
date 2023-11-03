810a7ccd4c2cb689c042b6dd6b228cb840f884cc
Remedy1995 <adjetadjetey45@gmail.com>
2023-11-03 08:59:29 +0000

    added the build files

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const express = require('express');
 require('dotenv').config();
 const path = require('path');
-const cors=require('cors');
+const cors = require('cors');
 const bodyparser = require('body-parser');
 const app = express();
-const port = process.env.PORT||3000;
-const contactUsEmail=require('./routes/contactEmail');
-const mapping=require('./routes/Map');
-const shipping=require('./routes/Shipping');
-const connection=require('./controller/Auth');
+const port = process.env.PORT || 3000;
+const contactUsEmail = require('./routes/contactEmail');
+const mapping = require('./routes/Map');
+const shipping = require('./routes/Shipping');
+const connection = require('./controller/Auth');
 connection();//database;
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname + '/public/dist/malacko')));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/public/dist/malacko/index.html'));});
-app.use('/shipping',shipping);
-app.use('/mapping',mapping);
-app.use('/contactUsEmail',contactUsEmail);
-app.use(bodyparser.urlencoded({extended:true}));
-app.get('/geocode', (req,res) => {})
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname +
+    '/public/dist/malacko/index.html'));
+});
+app.use('/shipping', shipping);
+app.use('/mapping', mapping);
+app.use('/contactUsEmail', contactUsEmail);
+app.use(bodyparser.urlencoded({ extended: true }));
+app.get('/geocode', (req, res) => { })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
